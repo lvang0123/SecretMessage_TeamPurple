cfb4e435742b0b1a3ac5677ede8cdbc11375caf8
lvang0123 <37590716+lvang0123@users.noreply.github.com>
2018-05-08 10:37:57 -0400

    Update main.py
    Added codes to prompt the user whether or not they want to decrypt their message. Additional comments was also added.

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
 #Python Team Project - Secret Message
 #This program will prompt the user for a message that will be encrypted. The user will then be asked to enter a key (number) to be used to determine which letters ahead of the letters in the message (user input) will be used. 
-#May 1, 2018 - Ben, Dan, Kyle, Lisa, and Lia all worked on this program together during class time, and collaboratively through Google Hangouts and GitHub.
+#May 1, 2018 - Ben, Dan, Kyle, Lisa, and Lia all worked on this program together during class time.
+
+#Welcomeing the user
+print("Welcome to the Secret Message program!")
+print()
 
 #Kyle - declaring variables
 #!/bin/python3
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 newMessage = ''
+decryptNo = "no"
+decryptYes = "yes"
 
 #Dan - prompt user to input message for encryption
-message = input('Please enter a message in lower-case letters.')
+while True:
+  message = input('Please enter a message in lowercase letters to be encrypted:')
+  break
 
 print()
 
-#Ben - prompt user to input key to encrypt
-key = input('Enter a key(1-26): ')
-key = int(key)
+#Ben - prompting the user to input key to encrypt
+while True:
+  try:
+    key = input('Enter a key(1-26): ')
+    key = int(key)
+    break
+  except ValueError:
+    print()
+    print("*** ERROR: Not an integer ***")
+    print()
 
 print()
 
@@ -36,21 +51,47 @@ for character in message:
 
 #outputs encrypted word
 print("Your new message is: ", newMessage)
-
-#reverse of encrypted message
-message = newMessage
-myMessage = ''
-for character in message:
-  if character in alphabet:
-    position = alphabet.find(character)
-    newPosition = (position - key) % 26
-    newCharacter = alphabet[newPosition]
-    myMessage += newCharacter
-    
 print()
-    
-#outputs decrypted word    
-print("Your decrypted message is: ", myMessage)
+
+#Prompting the user to decrypt the message or leave it alone
+while True:
+  print("Would you like to decrypt the message?")
+  decrypt = input("Yes or No: ")
+  
+  if decrypt.lower() == decryptYes:
+    break
+  elif decrypt.lower() == decryptNo:
+    print()
+    break
+  else:
+    print()
+    print("*** ERROR: Please enter 'Yes' or 'No' ***")
+    print()
+
+#Message will be decrypted if user inputted 'Yes'
+while decrypt.lower() == decryptYes:
+  #reverse of encrypted message
+  message = newMessage
+  oldMessage = ''
+  for character in message:
+    if character in alphabet:
+      position = alphabet.find(character)
+      newPosition = (position - key) % 26
+      newCharacter = alphabet[newPosition]
+      oldMessage += newCharacter
+      
+    else:
+      oldMessage += character
+      
+  print()
+      
+  #outputs the decrypted message   
+  print("Your decrypted message is:", oldMessage)
+  print()
+  break
+
+#Informing the user that the program has ended
+print("Thank you for using the Secret Message program!")
 
 
 
